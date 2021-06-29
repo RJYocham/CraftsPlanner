ed688e4d41120648a26c33adca82fae7e62e9114
Rhiannan Yocham <rjyocham@gmail.com>
2021-06-29 10:00:24 -0400

    set up Project class

---
+++ CraftsPlanner.Data/Project.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftsPlanner.Data
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public string Source { get; set; }
+
+        //public enum Categories { get; set; }
+        //public int Difficulty { get; set; }
+        //public List<Material> Materials { get; set; }
+        //public int Budget { get; set; }
+        //public int Cost { get; set; }
+        //public int Plan { get; set; }
+    }
+}
