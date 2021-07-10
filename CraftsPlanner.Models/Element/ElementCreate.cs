a6cacbcf9f99fdbf9cc73f8e80a2ca3c77a36bed
Rhiannan Yocham <rjyocham@gmail.com>
2021-07-10 15:55:05 -0400

    made empty classes and controllers for Project group and elements; (create, detail, edit, listitem, service, and controller)

---
+++ CraftsPlanner.Models/Element/ElementCreate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CraftsPlanner.Models.Element
+{
+    public class ElementCreate
+    {
+    }
+}
